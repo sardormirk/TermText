9df08a4b5ba12ecb846f17315d811b57b79c5f4d
Sardor Mirkomilov <89600047+voidblob@users.noreply.github.com>
2024-02-03 21:46:23 -0500

    most features implemented

---
+++ Build/CMakeFiles/TermText.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TermText.dir/TermText.c.o"
+  "CMakeFiles/TermText.dir/TermText.c.o.d"
+  "TermText"
+  "TermText.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TermText.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
